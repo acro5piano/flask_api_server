8c564d2521ea527aa6717753eecbcb66f8b6f0c9
gosho-kazuya <ketsume0211@gmail.com>
2016-03-21 17:04:08 +0900

    add api function

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ simple api server with Flask
 # Installation
 
 - Install python-pip,virtualenv in advance.
+- `sudo apt-get install python-pip python-dev libmysqlclient-dev mysql-server`
+- `sudo pip install virtualenv`
 - Exec `bash setup.sh`
+- Create Mysql Database named 'api_server'.
+- Change config.py for your environment.
 
 
--- api-server.py
+++ api-server.py
@@ -1,29 +1,49 @@
 #!/usr/bin/env python
 
-from flask import Flask
-import read_press
+from flask import Flask, jsonify, render_template
+from flask.ext.mysql import MySQL
 
-app= Flask(__name__)
+import config
+
+app = Flask(__name__)
+
+# MySQL configurations
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER']     = config.DB_USER
+app.config['MYSQL_DATABASE_PASSWORD'] = config.DB_PASS
+app.config['MYSQL_DATABASE_DB']       = config.DB_NAME
+app.config['MYSQL_DATABASE_HOST']     = config.DB_HOST
+mysql.init_app(app)
 
 @app.route('/')
 def hello_world():
-	return str(read_press.get_pressure())
+	return render_template('index.html')
 
 @app.route('/api')
 def api_hello():
-	return '{"message":"thanks!"}'
+	result = { "message":"welcome to Flask Api Server!" }
+	return jsonify(result)
 
-@app.route('/api/stand')
-def stand():
-	return True
+@app.route('/api/stand_up')
+def stand_up():
+	query = 'insert into chair_log( action, inserted_at ) values( \'stand_up\', NOW() )'
+	execute(query)
+	return jsonify({"message":"ok","action":"stand_up"})
 
-@app.route('/api/sit')
-def sit():
-	return True
+@app.route('/api/sit_down')
+def sit_down():
+	query = 'insert into chair_log( action, inserted_at ) values( \'sit_down\', NOW() )'
+	execute(query)
+	return jsonify({"message":"ok","action":"sit_down"})
 
-
+def execute(query):
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute(query)
+	conn.commit()
 
 if __name__ =='__main__':
+	execute('create table if not exists chair_log(id int primary key auto_increment, action varchar(20), inserted_at datetime)')
 	app.run(host='0.0.0.0',debug=True)
 
 
