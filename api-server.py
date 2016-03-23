0e5067c3eeec94d472351fa1ea6b4eb0f8141de5
gosho-kazuya <ketsume0211@gmail.com>
2016-03-23 20:26:14 +0900

    create method is_music_play

--- api-server.py
+++ api-server.py
@@ -2,9 +2,10 @@
 
 from flask import Flask, jsonify, render_template, request
 from flask.ext.mysql import MySQL
-
-import config
 import random
+
+# config file 
+import config
 
 app = Flask(__name__)
 
@@ -25,10 +26,10 @@ def hello_api():
 	result = { "message":"welcome to Flask Api Server!" }
 	return jsonify(result)
 
+# api for rapberry pi
 @app.route('/api/chair_log',methods=['POST'])
 def chair_log():
 	value = int(request.form['value'])
-	print(value)
 	query = 'insert into chair_log( action, inserted_at ) values( %d , NOW() )' % ( value )
 	execute(query)
 	return jsonify({"message":"ok","action":"stand_up"})
@@ -36,11 +37,19 @@ def chair_log():
 def execute(query):
 	conn = mysql.connect()
 	cursor = conn.cursor()
-	cursor.execute(query)
+	res = cursor.execute(query)
 	conn.commit()
+	return res
+
+# api to browser
+@app.route('/api/is_music_play')
+def is_music_play():
+	res = execute('select * from (select * from chair_log order by inserted_at desc limit 12) as L where L.action = 0') ;	
+	res = 0 if res > 0 else 1
+	return jsonify({"music_play":res})
 
 @app.route('/api/is_music_play/random')
-def is_music_play():
+def is_music_play_random():
 	rand = random.randint(0,1)
 	return jsonify({"music_play":rand})
 
